e0fd749b31441160b9a2b70ed2a41d7216619bab
drako9159 <chochito9159@gmail.com>
2024-12-20 14:14:12 -0600

    feat(args): recibe args for send emails

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from dotenv import load_dotenv
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 import logging
+import argparse
 
 # configure logging
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
@@ -12,20 +13,26 @@ logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(level
 def load_credentials():
     try:
         load_dotenv()
-        return os.getenv('EMAIL'), os.getenv('PASSWORD')
+        email = os.getenv('EMAIL')
+        password = os.getenv('PASSWORD')
+        if not email or not password:
+            raise ValueError("Email or password not found")
+        return email, password
     except Exception as e:
-        logging.error(f"An error occurred: {e}")
-        return None
+        logging.error(f"An error occurred while loading credentials: {e}")
+        return None, None
 
 # load file html
 def load_email_template(file_path):
     try:
         with open(file_path, "r", encoding="utf-8") as f:
             return f.read()
+    except FileNotFoundError:
+        logging.error(f"File not found: {file_path}")
+        raise
     except Exception as e:
-        raise FileNotFoundError(f"File not found: {file_path}")
-    except Exception as e:
-        raise Exception(f"An error occurred: {e}")
+        logging.error(f"An loading template {file_path}: {e}")
+        raise
 
 # send email
 def send_email(addressee, affair, html_file):
@@ -48,13 +55,21 @@ def send_email(addressee, affair, html_file):
             server.sendmail(sender, addressee, msg.as_string())
             server.quit()
             logging.info("Email sent successfully")
-    except Exception as e:
-        logging.error(f"An error occurred: {e}")
+    except smtplib.SMTPException as e:
+        logging.error(f"SMTP error occurred: {e}")
         raise
 
 if __name__ == "__main__":
+    # Argument parser
+    parser = argparse.ArgumentParser(description="Send email with a subject and a html file")
+    parser.add_argument("-e", "--email", required=True, help="Recipient email address.")
+    parser.add_argument("-s", "--subject", required=True, help="Subject of the email.")
+    parser.add_argument("-f", "--file", required=True, help="Path to the HTML file to send.")
+
+    args = parser.parse_args()
+
     try:
-        send_email("mail@mail.com", "Test", "email.html")
+        send_email(args.email, args.subject, args.file)
     except Exception as e:
         logging.error(f"An error occurred: {e}")
 
